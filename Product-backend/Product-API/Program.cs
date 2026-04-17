6b07d98de79cd48688546400fc621729fab7426d
krzysztof.klich <krzysztof.klich@idealogic.pl>
2026-04-17 11:48:17 +0200

    add fluent validation

--- Product-backend/Product-API/Program.cs
+++ Product-backend/Product-API/Program.cs
@@ -1,9 +1,15 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Product_API.Application.Mappings;
 using Product_API.Application.Services;
 using Product_API.Infrastructure.Repositories;
 using Product_API.Middlewares;
+using Product_API.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateProductValidator>();
 
 builder.Services.AddControllers();
 
